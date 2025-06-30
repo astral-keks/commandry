a3e03916f8623f3f715b41a5e7676488113d9278
Nikita Demidchik <astralkeks@gmail.com>
2025-06-30 22:15:05 +0300

    Support all MCP features and primitives

---
+++ src/Commandry.Mcp.Sdk/InvokeElicitationCmdlet.cs
@@ -0,0 +1,25 @@
+﻿using ModelContextProtocol.Protocol;
+using ModelContextProtocol.Server;
+using System.Management.Automation;
+
+namespace Commandry
+{
+    [Cmdlet(VerbsLifecycle.Invoke, "Elicitation")]
+    [OutputType(typeof(ElicitResult))]
+    public class InvokeElicitationCmdlet : PSCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        [ValidateNotNull]
+        public ElicitRequestParams? Request { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            if (this.TryGetMcpServer(out IMcpServer? mcp) && Request is not null)
+            {
+                ElicitResult result = mcp.ElicitAsync(Request).GetAwaiter().GetResult();
+                
+                WriteObject(result);
+            }
+        }
+    }
+}
